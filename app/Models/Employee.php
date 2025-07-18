ee5c5354c6ea3a133ba64f83724c46aae187e4be
Karungg <miftahfadilah71@gmail.com>
2025-07-19 05:45:38 +0700

    Add migration, model employee

---
+++ app/Models/Employee.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Employee extends Model
+{
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'employee_id',
+        'name',
+        'address',
+        'user_id'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+}
