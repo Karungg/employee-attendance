a0d3ddf2970c4c65449cf2fe244e96692e55ef7d
Karungg <miftahfadilah71@gmail.com>
2025-07-19 08:14:08 +0700

    Add departement resource

---
+++ app/Filament/Resources/DepartementResource.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\DepartementResource\Pages;
+use App\Filament\Resources\DepartementResource\RelationManagers;
+use App\Models\Departement;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class DepartementResource extends Resource
+{
+    protected static ?string $model = Departement::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('departement_name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('max_clock_in_time')
+                    ->required(),
+                Forms\Components\TextInput::make('max_clock_out_time')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('departement_name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('max_clock_in_time'),
+                Tables\Columns\TextColumn::make('max_clock_out_time'),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListDepartements::route('/'),
+            'create' => Pages\CreateDepartement::route('/create'),
+            'view' => Pages\ViewDepartement::route('/{record}'),
+            'edit' => Pages\EditDepartement::route('/{record}/edit'),
+        ];
+    }
+}
